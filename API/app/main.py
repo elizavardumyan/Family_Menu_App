3586b385febe3c95100a0ca343557bf1b2e38d86
Eliza Vardumyan <eliz.vardumyan@gmail.com>
2026-08-09 14:34:21 +0400

    Connect FastAPI to PostgreSQL and add recipes endpoint

--- API/app/main.py
+++ API/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from .database import get_connection
 
 app = FastAPI(
     title="Family Menu API",
@@ -10,3 +11,26 @@ def root():
     return {
         "message": "Welcome to Family Menu API!"
     }
+    from .database import get_connection
+
+
+@app.get("/recipes")
+def get_recipes():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                SELECT recipe_id, recipe_code, base_servings
+                FROM recipes
+                ORDER BY recipe_id;
+            """)
+
+            rows = cur.fetchall()
+
+    return [
+        {
+            "recipe_id": row[0],
+            "recipe_code": row[1],
+            "base_servings": row[2]
+        }
+        for row in rows
+    ]
